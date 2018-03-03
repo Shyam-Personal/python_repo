e2c2a7d3760c342ffbd17cc149f174f09bf36743
Shyam Deshmukh <shyamdeshmukh1@gmail.com>
2018-03-03 22:46:34 +0530

    Added email id validation regex

--- Python Practice/ip_address_regex.py
+++ Python Practice/ip_address_regex.py
@@ -6,3 +6,16 @@ if(re.match("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]
 	print("Valid IP")
 else:
 	print("Invalid IP")
+	
+a = "shyam.deshmukh@gmail.com"
+b = "shyam.deshmukh@gmail.com!"
+
+if (re.match(r"^[\w._]+\@[\w._]+\.[\w._]+$", a)):
+	print("Valid email " + a)
+else:
+	print("Invalid Email " + a)
+	
+if (re.match(r"^[\w._]+\@[\w._]+\.[\w._]+$", b)):
+	print("Valid email " + b)
+else:
+	print("Invalid Email " + b)
